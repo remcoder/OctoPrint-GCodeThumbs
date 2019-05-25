994a04c19f27ac88a314066b7f054a977b1f18ef
Remco Veldkamp <remcoder@gmail.com>
2019-05-26 00:26:24 +0200

    Fix promise error handler

--- octoprint_gcodethumbs/static/js/gcode-preview-viewmodel.js
+++ octoprint_gcodethumbs/static/js/gcode-preview-viewmodel.js
@@ -90,7 +90,7 @@ $(function() {
             insertAfter(canvas, element.querySelector('.title'))
           }
         })
-        .error(function() {
+        .catch(function() {
           downloading = false;
         });
     }
